5c8cd8582143b4203ccd68369861f1d0ca5528a0
Ssebaaa <nicholassebastiani70@gmail.com>
2026-03-18 16:19:03 +0100

    creazione files con metodi principali da riempire

---
+++ Enemy.java
@@ -0,0 +1,7 @@
+public class Enemy extends Character {
+    public int rewardXP;
+
+    public void takeTurn() {
+        // Logica dell'IA del nemico
+    }
+}
---
+++ Unit734.java
@@ -0,0 +1,6 @@
+public class Unit734 extends Player {
+    
+    public void overclock() {
+        // Abilità specifica di Unit734
+    }
+}
